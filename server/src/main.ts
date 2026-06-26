6dbf0e70125093551cd9de7f29a168941a03db44
The Anh <theanhtong022@gmail.com>
2026-06-26 16:23:22 +0700

    feat: add Swagger with CLI plugin

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,24 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module.js';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('NestJS Ecommerce Backend')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
