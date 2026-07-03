5d6c65aafd269e45cf36012b1879d706ec92c180
Tong The Anh <theanhtong022@gmail.com>
2026-07-03 14:20:25 +0700

    chore: setup initial frontend project structure

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
